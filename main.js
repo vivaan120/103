b3ecf3ef7d28f142b831d70c7fe22c1ca65b005c
vivaan120 <83453732+vivaan120@users.noreply.github.com>
2021-09-19 19:55:58 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+
+Webcam.set({
+    height:300,
+    width:400,
+    image_format:"png",
+    png_quality:90
+   });
+   camera=document.getElementById("camera");
+   Webcam.attach(camera);
+
+
+    
